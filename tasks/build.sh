e89266300f02542d96e28f9c20908c9d7e8928b5
SwapnilSankla <swapnil.sankla@gmail.com>
2017-09-18 18:58:42 +0530

    Updated zip command to recursively zip things

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,3 +1,3 @@
 xcodebuild clean build  -project RetailStore.xcodeproj -scheme RetailStore -destination 'platform=iOS Simulator,OS=10.3.1,name=iPhone 6s' CONFIGURATION_BUILD_DIR='./build'
 cd build
-zip RetailStore.zip RetailStore.app
+zip -r RetailStore.zip RetailStore.app
