dd6c74a81a8d76a714943737b338a8ede8f12f08
SwapnilSankla <swapnil.sankla@gmail.com>
2017-09-18 21:32:52 +0530

    Added xcpretty to generate test report

--- tasks/unitTest.sh
+++ tasks/unitTest.sh
@@ -1,1 +1,1 @@
-xcodebuild test  -project RetailStore.xcodeproj -scheme RetailStore -destination 'platform=iOS Simulator,OS=10.3.1,name=iPhone 6s' CONFIGURATION_BUILD_DIR='./build' -enableCodeCoverage YES
+xcodebuild test  -project RetailStore.xcodeproj -scheme RetailStore -destination 'platform=iOS Simulator,OS=10.3.1,name=iPhone 6s' CONFIGURATION_BUILD_DIR='./build' -enableCodeCoverage YES | xcpretty -r html
