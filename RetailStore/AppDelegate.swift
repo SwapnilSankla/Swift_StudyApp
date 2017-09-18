9389e3a20e8682c4fc6dfc10a57fa51ad9e6ee02
SwapnilSankla <swapnil.sankla@gmail.com>
2017-09-18 19:49:02 +0530

    Intentionally pushing error

--- RetailStore/AppDelegate.swift
+++ RetailStore/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
-        let productsController = ProductsController(style: .plain)
+        let productsController = new ProductsController(style: .plain)
         window?.rootViewController = productsController
         window?.makeKeyAndVisible()
         return true
