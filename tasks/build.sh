5aba42bdf1b9e7fd6bf19eb64f06194b178c0c6f
SwapnilSankla <swapnil.sankla@gmail.com>
2017-09-18 20:08:13 +0530

    Updated build script to catch build failures

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,3 +1,7 @@
-xcodebuild clean build  -project RetailStore.xcodeproj -scheme RetailStore -destination 'platform=iOS Simulator,OS=10.3.1,name=iPhone 6s' CONFIGURATION_BUILD_DIR='./build'
-cd build
-zip -r RetailStore.zip RetailStore.app
+if xcodebuild clean build  -project RetailStore.xcodeproj -scheme RetailStore -destination 'platform=iOS Simulator,OS=10.3.1,name=iPhone 6s' CONFIGURATION_BUILD_DIR='./build'
+then
+ cd build
+ zip -r RetailStore.zip RetailStore.app
+else
+ exit 1
+fi
