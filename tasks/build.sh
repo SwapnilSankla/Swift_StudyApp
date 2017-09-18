c87efd18b14f51d7e2d8f9c335fbeb82541c6568
SwapnilSankla <swapnil.sankla@gmail.com>
2017-09-18 16:48:36 +0530

    Corrected build script

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,1 +1,1 @@
-xcodebuild clean build  -project ../RetailStore.xcodeproj -scheme RetailStore -destination 'platform=iOS Simulator,OS=10.3.1,name=iPhone 6s' CONFIGURATION_BUILD_DIR='../build'
+xcodebuild clean build  -project RetailStore.xcodeproj -scheme RetailStore -destination 'platform=iOS Simulator,OS=10.3.1,name=iPhone 6s' CONFIGURATION_BUILD_DIR='./build'
