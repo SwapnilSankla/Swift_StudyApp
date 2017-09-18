3f827e9e439da76c4400fea24b862674dafa173a
SwapnilSankla <swapnil.sankla@gmail.com>
2017-09-18 18:19:07 +0530

    Zipping .app file as on GoCD it appears as a directory

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,1 +1,2 @@
 xcodebuild clean build  -project RetailStore.xcodeproj -scheme RetailStore -destination 'platform=iOS Simulator,OS=10.3.1,name=iPhone 6s' CONFIGURATION_BUILD_DIR='./build'
+zip -r ./build/RetailStore.zip ./build/RetailStore.app
