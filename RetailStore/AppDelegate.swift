5371cdf3c81e0c8fc3dc2c308d68a3cf15383cab
SwapnilSankla <swapnil.sankla@gmail.com>
2017-09-18 20:13:12 +0530

    Revert "Intentionally pushing error"
    This reverts commit 9389e3a20e8682c4fc6dfc10a57fa51ad9e6ee02.

--- RetailStore/AppDelegate.swift
+++ RetailStore/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
-        let productsController = new ProductsController(style: .plain)
+        let productsController = ProductsController(style: .plain)
         window?.rootViewController = productsController
         window?.makeKeyAndVisible()
         return true
